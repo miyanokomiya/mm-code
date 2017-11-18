1bbe007b7581cf03a910611c9926434286ddb956
miyanokomiya <komiyamatomoya@mbp.local>
2017-11-18 15:30:35 +0900

    エディター部分をコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,41 @@
 <template>
   <div id="app">
-    <h3>{{fileName || 'NO FILE'}}</h3>
-    <div ref="editorWrapper" class="editor-wrapper">
-      <div class="line-count-wrapper">
-        <ul class="line-count">
-          <li v-for="(l, i) in lines" :key="i">
-            <span ref="lineHighlight" v-if="i === cursor.row" class="line-highlight"></span>
-            <span ref="cursor" class="cursor" v-if="i === cursor.row"></span>
-            <span>{{i}}</span>
-          </li>
-        </ul>
-      </div>
-      <pre :class="`language-${languageType}`"><code v-html="prismHtml" :class="`language-${languageType}`"></code></pre>
-    </div>
+    <h3>{{activeEditorName || 'NO FILE'}}</h3>
+    <EditorBox
+      v-if="activeEditor"
+      :fileName="activeEditor.fileName"
+      :lines="activeEditor.lines"
+      :cursor="activeEditor.cursor"
+    />
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import Prism from 'prismjs'
-import {languageType} from './commons/prismConfig'
+import EditorBox from './components/EditorBox'
 
 let websocket = null
 let connectionLoop = null
 
 export default {
-  name: 'this',
+  components: {
+    EditorBox
+  },
   data () {
     return {
-      fileName: '',
-      lines: [],
-      cursor: {
-        row: 0,
-        column: 0
-      }
+      activeEditorName: null,
+      editors: {}
     }
   },
   computed: {
-    prismHtml () {
-      const prismHtml = this.filterPrism(this.convertedText)
-      return prismHtml
-    },
-    convertedText () {
-      return this.lines.join('\n')
-    },
-    languageType () {
-      return languageType(this.fileName)
+    activeEditor () {
+      return this.editors[this.activeEditorName]
     }
   },
   mounted () {
     this.initSocket()
   },
-  updated () {
-    if (this.$refs.editorWrapper) {
-      const width = this.$refs.editorWrapper.scrollWidth
-      if (this.$refs.lineHighlight.length > 0) {
-        this.$refs.lineHighlight[0].style.width = `${width + 30}px`
-      }
-      if (this.$refs.cursor.length > 0) {
-        // フォントサイズとパディングを考慮して絶妙な位置に調整
-        this.$refs.cursor[0].style.left = `${13 + this.cursor.column * 9.6}px`
-      }
-    }
-  },
   methods: {
-    filterPrism (text) {
-      let ret = text
-      const type = Prism.languages[this.languageType]
-      if (type) {
-        ret = Prism.highlight(text, type)
-      }
-      return ret
-    },
     initSocket () {
       if (websocket) {
         websocket.close()
@@ -104,35 +68,47 @@ export default {
 
       if (type === 'say') {
       } else if (type === 'text') {
-        this.fileName = jsonData.fileName
-        this.lines = jsonData.text.split('\n')
+        this.activeEditorName = jsonData.fileName
+        Vue.set(this.editors, jsonData.fileName, {
+          fileName: jsonData.fileName,
+          lines: jsonData.text.split('\n'),
+          cursor: {
+            row: 0,
+            column: 0
+          }
+        })
       } else if (type === 'line') {
-        Vue.set(this.lines, parseInt(jsonData.r), jsonData.l)
-        this.cursor.row = parseInt(jsonData.r)
-        this.cursor.column = parseInt(jsonData.c)
+        const editor = this.activeEditor
+        if (editor) {
+          Vue.set(editor.lines, parseInt(jsonData.r), jsonData.l)
+          editor.cursor.row = parseInt(jsonData.r)
+          editor.cursor.column = parseInt(jsonData.c)
+        }
       } else if (type === 'updates') {
         // 複数行更新
-        jsonData.updates.forEach((update) => {
-          let stackText = update.t
+        const editor = this.activeEditor
+        if (editor) {
+          jsonData.updates.forEach((update) => {
+            let stackText = update.t
+            // テキスト全体からみた変更箇所
+            let start = 0
+            let end = 0
+            for (let i = 0; i < update.sr; i++) {
+              start += editor.lines[i].length + 1
+              end = start
+            }
+            start += update.sc
 
-          // テキスト全体からみた変更箇所
-          let start = 0
-          let end = 0
-          for (let i = 0; i < update.sr; i++) {
-            start += this.lines[i].length + 1
-            end = start
-          }
-          start += update.sc
+            for (let i = update.sr; i < update.er; i++) {
+              end += editor.lines[i].length + 1
+            }
+            end += update.ec
 
-          for (let i = update.sr; i < update.er; i++) {
-            end += this.lines[i].length + 1
-          }
-          end += update.ec
-
-          let text = this.lines.join('\n')
-          let newText = text.slice(0, start) + stackText + text.slice(end)
-          this.lines = newText.split('\n')
-        })
+            let text = editor.lines.join('\n')
+            let newText = text.slice(0, start) + stackText + text.slice(end)
+            editor.lines = newText.split('\n')
+          })
+        }
       } else if (type === 'join') {
         // 誰かが入室
         console.log('new comer')
@@ -160,8 +136,6 @@ export default {
 </script>
 
 <style lang="scss">
-$back-color: #272822;
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -169,86 +143,5 @@ $back-color: #272822;
   text-align: center;
   color: #2c3e50;
   margin-top: 20px;
-}
-
-.editor-wrapper {
-  overflow: auto;
-  background-color: $back-color;
-
-  pre[class*="language-"] {
-    overflow: initial;
-    margin: 0;
-    padding: 1em 24px 1.8em 44px;
-  }
-  code[class*="language-"] {
-    padding-right: 24px;
-  }
-}
-
-.line-count-wrapper {
-  float: left;
-  padding: 20px 0 0 0;
-  color: #FFF;
-  border-radius: 4px;
-}
-
-.line-count {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.line-count li {
-  width: 30px;
-  text-align: right;
-  font-size: 0.8rem;
-  line-height: 1.5;
-  height: 1.5rem;
-  position: relative;
-}
-
-.line-highlight {
-  display: inline-block;
-  position: absolute;
-  height: 100%;
-  width: 300px;
-  top: -6px;
-  left: 0;
-  pointer-events: none;
-  // background-color: rgba(0, 0, 255, 0.2);
-  border-bottom: 2px solid rgba(255, 255, 255, 0.8);
-}
-
-
-.cursor {
-  float: right;
-  position: relative;
-  border-left: 2px solid #fff;
-  margin: 0 -2px 0 0;
-  display: inline-block;
-  height: 1rem;
-  vertical-align: middle;
-
-  -webkit-animation:blink 0.5s ease-in-out infinite alternate;
-  -moz-animation:blink 0.5s ease-in-out infinite alternate;
-  animation:blink 0.5s ease-in-out infinite alternate;
-  @-webkit-keyframes blink{
-    0% {opacity:1;}
-    50% {opacity:1;}
-    51% {opacity:0;}
-    100% {opacity:0;}
-  }
-  @-moz-keyframes blink{
-    0% {opacity:1;}
-    50% {opacity:1;}
-    51% {opacity:0;}
-    100% {opacity:0;}
-  }
-  @keyframes blink{
-    0% {opacity:1;}
-    50% {opacity:1;}
-    51% {opacity:0;}
-    100% {opacity:0;}
-  }
 }
 </style>
---
+++ src/components/EditorBox.vue
@@ -0,0 +1,162 @@
+<template>
+  <div ref="editorWrapper" class="editor-wrapper">
+    <div class="line-count-wrapper">
+      <ul class="line-count">
+        <li v-for="(l, i) in lines" :key="i">
+          <span ref="lineHighlight" v-if="i === cursor.row" class="line-highlight"></span>
+          <span ref="cursor" class="cursor" v-if="i === cursor.row"></span>
+          <span>{{i}}</span>
+        </li>
+      </ul>
+    </div>
+    <pre :class="`language-${languageType}`"><code v-html="prismHtml" :class="`language-${languageType}`"></code></pre>
+  </div>
+</template>
+
+<script>
+import Prism from 'prismjs'
+import {languageType} from '@/commons/prismConfig'
+
+export default {
+  name: 'this',
+  props: {
+    fileName: {
+      type: String,
+      default: 'no name'
+    },
+    lines: {
+      type: Array,
+      default: () => []
+    },
+    cursor: {
+      row: {
+        type: Number,
+        default: 0
+      },
+      column: {
+        type: Number,
+        default: 0
+      }
+    }
+  },
+  computed: {
+    prismHtml () {
+      const prismHtml = this.filterPrism(this.convertedText)
+      return prismHtml
+    },
+    convertedText () {
+      return this.lines.join('\n')
+    },
+    languageType () {
+      return languageType(this.fileName)
+    }
+  },
+  updated () {
+    if (this.$refs.editorWrapper) {
+      const width = this.$refs.editorWrapper.scrollWidth
+      if (this.$refs.lineHighlight.length > 0) {
+        this.$refs.lineHighlight[0].style.width = `${width}px`
+      }
+      if (this.$refs.cursor.length > 0) {
+        // フォントサイズとパディングを考慮して絶妙な位置に調整
+        this.$refs.cursor[0].style.left = `${13 + this.cursor.column * 9.6}px`
+      }
+    }
+  },
+  methods: {
+    filterPrism (text) {
+      let ret = text
+      const type = Prism.languages[this.languageType]
+      if (type) {
+        ret = Prism.highlight(text, type)
+      }
+      return ret
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$back-color: #272822;
+
+.editor-wrapper {
+  overflow: auto;
+  background-color: $back-color;
+
+  pre[class*="language-"] {
+    overflow: initial;
+    margin: 0;
+    padding: 1em 24px 1.8em 44px;
+  }
+  code[class*="language-"] {
+    padding-right: 24px;
+  }
+}
+
+.line-count-wrapper {
+  float: left;
+  padding: 20px 0 0 0;
+  color: #FFF;
+  border-radius: 4px;
+}
+
+.line-count {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.line-count li {
+  width: 30px;
+  text-align: right;
+  font-size: 0.8rem;
+  line-height: 1.5;
+  height: 1.5rem;
+  position: relative;
+}
+
+.line-highlight {
+  display: inline-block;
+  position: absolute;
+  height: 100%;
+  width: 300px;
+  top: -6px;
+  left: 0;
+  pointer-events: none;
+  // background-color: rgba(0, 0, 255, 0.2);
+  border-bottom: 2px solid rgba(255, 255, 255, 0.8);
+}
+
+
+.cursor {
+  float: right;
+  position: relative;
+  border-left: 2px solid #fff;
+  margin: 0 -2px 0 0;
+  display: inline-block;
+  height: 1rem;
+  vertical-align: middle;
+
+  -webkit-animation:blink 0.5s ease-in-out infinite alternate;
+  -moz-animation:blink 0.5s ease-in-out infinite alternate;
+  animation:blink 0.5s ease-in-out infinite alternate;
+  @-webkit-keyframes blink{
+    0% {opacity:1;}
+    50% {opacity:1;}
+    51% {opacity:0;}
+    100% {opacity:0;}
+  }
+  @-moz-keyframes blink{
+    0% {opacity:1;}
+    50% {opacity:1;}
+    51% {opacity:0;}
+    100% {opacity:0;}
+  }
+  @keyframes blink{
+    0% {opacity:1;}
+    50% {opacity:1;}
+    51% {opacity:0;}
+    100% {opacity:0;}
+  }
+}
+</style>
